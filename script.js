19ceb90adc7673578f3cffa9987186da1767e35f
puchatechmurki <66203520+puchatechmurki@users.noreply.github.com>
2022-10-04 17:36:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const resetBtn = document.querySelector(".reset");
 const stopWatch = document.querySelector(".stop-watch");
 const time = document.querySelector(".time");
 const timeList = document.querySelector(".time-list");
+const historyBtn = document.querySelector(".history");
 
 const infoBtn = document.querySelector(".info");
 const modalShadow = document.querySelector(".modal-shadow");
@@ -14,7 +15,7 @@ let countTime;
 let minutes = 0;
 let seconds = 0;
 
-let timesArray = []
+let timesArray = [];
 
 const handleStart = () => {
 	clearInterval(countTime);
@@ -39,20 +40,61 @@ const handlePause = () => {
 };
 
 const handleStop = () => {
-    time.innerHTML = `Ostatni czas: ${stopWatch.textContent}`
+	time.innerHTML = `Ostatni czas: ${stopWatch.textContent}`;
 
-    if(stopWatch.textContent !== `0:00`){
-        time.style.visibility = 'visible'
-        timesArray.push(stopWatch.textContent)
-    }
+	if (stopWatch.textContent !== `0:00`) {
+		time.style.visibility = "visible";
+		timesArray.push(stopWatch.textContent);
+	}
 
+	clearStuff();
+};
+
+const handleReset = () => {
+	time.style.visibility = "hidden";
+	timesArray = [];
+	clearStuff();
+};
+
+const clearStuff = () => {
 	clearInterval(countTime);
 	stopWatch.textContent = `0:00`;
-	timeList.textContent = `0:00`;
+	timeList.textContent = ``;
 	seconds = 0;
 	minutes = 0;
 };
 
+const showHistory = () => {
+	timeList.textContent = ``;
+	let num = 1;
+
+	timesArray.forEach(time => {
+		const newTime = document.createElement("li");
+		newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`;
+
+		timeList.appendChild(newTime);
+		num++;
+	});
+};
+
+const showModal = () => {
+	if (!(modalShadow.style.display === "block")) {
+		modalShadow.style.display = "block";
+	} else {
+		modalShadow.style.display = "none";
+	}
+
+    modalShadow.classList.toggle('modal-animation')
+};
+
+
+
 startBtn.addEventListener("click", handleStart);
 pauseBtn.addEventListener("click", handlePause);
 stopBtn.addEventListener("click", handleStop);
+resetBtn.addEventListener("click", handleReset);
+historyBtn.addEventListener("click", showHistory);
+
+infoBtn.addEventListener("click", showModal);
+closeModalBtn.addEventListener('click', showModal)
+window.addEventListener('click', e => e.target === modalShadow ? showModal() : false)
